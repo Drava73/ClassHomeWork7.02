f192e3f7debf04e1fbe96b65a7d4ac26d095add5
My Name <kiykobogdan@gmail.com>
2023-02-07 15:14:48 +0200

    Add project files.

---
+++ ClassHomeWork7.02/Webs.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassHomeWork7._02
+{
+    public class Webs
+    {
+        private string nameweb;
+        private string webaddrs;
+        private string about;
+        private string ipweb;
+        public Webs()
+        {
+            nameweb = "Null";
+            webaddrs = "Null";
+            about = "Null";
+            ipweb = "Null";
+        }
+        public Webs(string nameweb, string webaddrs, string about, string ipweb){
+            this.nameweb = nameweb;
+            this.webaddrs = webaddrs;
+            this.about = about;
+            this.ipweb = ipweb;
+
+        }
+        public string WebSite
+        {
+            get { return nameweb; }
+            set { nameweb = value; }
+        }
+        public string WebAddres
+        {
+            get { return webaddrs; }
+            set { webaddrs = value; }
+        }
+        public string AboutSite
+        {
+            get { return about; }
+            set { about = value; }
+        }
+        public string IP
+        {
+            get { return ipweb; }
+            set { ipweb = value; }
+        }
+        public void Input() {
+            Console.WriteLine("Input Name:");
+            nameweb = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("Input Address:");
+            webaddrs = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("Input about site:");
+            about = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("Input IP:");
+            ipweb = Convert.ToString(Console.ReadLine());
+        }
+        public override string ToString()
+        {
+            return $"Name:{nameweb}\nAddress:{webaddrs}\nAbout site::{about}\nIP:{ipweb}";
+        }
+    }
+}
